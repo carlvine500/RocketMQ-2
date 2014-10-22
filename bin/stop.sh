af4b2d6db3a03039068a016d7743ab74f9897bf4
Zhanhui Li <lizhanhui@gmail.com>
2014-10-22 15:40:17 +0800

    update shell

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-for pid in `jps|awk '{if ($2 != "Jps") print $1}'`
-do
-   kill -15 ${pid}
-done
 
+function stop() {
+    if [ $# > 1 ]; then
+        kill -15 $1;
+    else
+        for pid in `jps|awk '{if ($2 != "Jps") print $1}'`
+        do
+           kill -15 ${pid}
+        done
+    fi
+}
+
+stop $@;
