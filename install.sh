fa5d1684faaeda02ca74cbcfa14a4f0061b7cef8
Zhanhui Li <lizhanhui@gmail.com>
2014-10-22 11:06:48 +0800

    copy sunjce_provider.jar

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ export PATH=$M2_HOME/bin:$JAVA_HOME/bin:$PATH
 mvn -Dmaven.test.skip=true clean package install assembly:assembly -U
 
 ln -s target/alibaba-rocketmq-3.1.9/alibaba-rocketmq devenv
+cp ${JAVA_HOME}/jre/lib/ext/sunjce_provider.jar devenv/lib/
